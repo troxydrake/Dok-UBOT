e563e44d8b24208dc5d8c5e591d38e3a1e9708fe
Vyougve <159968007+Ishotasf@users.noreply.github.com>
2024-02-28 04:47:20 +0700

    Update _ultroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -14,21 +14,21 @@ from telethon.errors import (
 from . import LOG_CHANNEL, LOGS, Button, asst, eor, get_string, ultroid_cmd
 
 REPOMSG = """
-• **ULTROID USERBOT** •\n
-• Repo - [Click Here](https://github.com/TeamUltroid/Ultroid)
-• Addons - [Click Here](https://github.com/TeamUltroid/UltroidAddons)
-• Support - @UltroidSupportChat
+▢ **RYN USERBOT** •\n
+▢ **Owner** - [Click Here](https://t.me/Usern4meDoestExist404)
+▢ **Channel** - [Click Here](https://t.me/Rxvthv)
+▢ **Support** - @CariSahabatOnline_Id
 """
 
 RP_BUTTONS = [
     [
-        Button.url(get_string("bot_3"), "https://github.com/TeamUltroid/Ultroid"),
-        Button.url("Addons", "https://github.com/TeamUltroid/UltroidAddons"),
+        Button.url(get_string("bot_3"), "https://t.me/Usern4meDoestExist404"),
+        Button.url("**Support Channel**", "https://t.me/Rxvthv"),
     ],
-    [Button.url("Support Group", "t.me/UltroidSupportChat")],
+    [Button.url("**Support Group**", "t.me/CariSahabatOnline_Id")],
 ]
 
-ULTSTRING = """🎇 **Thanks for Deploying Ultroid Userbot!**
+ULTSTRING = """▢ **Thanks for Deploying Ryn Userbot!**
 
 • Here, are the Some Basic stuff from, where you can Know, about its Usage."""
 
@@ -59,7 +59,7 @@ async def useUltroid(rs):
     msg = await asst.send_message(
         LOG_CHANNEL,
         ULTSTRING,
-        file="https://graph.org/file/54a917cc9dbb94733ea5f.jpg",
+        file="https://telegra.ph/file/72095c8ca8ee523ccc177.jpg",
         buttons=button,
     )
     if not (rs.chat_id == LOG_CHANNEL and rs.client._bot):
