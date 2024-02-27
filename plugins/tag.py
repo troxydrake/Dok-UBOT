60c569dd9c3b74ac34104b7d1e28a186885f9231
Vyougve <159968007+Ishotasf@users.noreply.github.com>
2024-02-28 03:58:57 +0700

    Update tag.py

--- plugins/tag.py
+++ plugins/tag.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 """
-✘✅Commands Available -
+✅Commands Available -
 
 ▢ `{i}tagall`
     Tag Top 100 Members of chat.
